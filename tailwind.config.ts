9371afd307473269b2d8c0b42d18c3508806f16b
Albin Shaji <albinalby24@gmail.com>
2025-11-04 04:31:08 +0000

    Remove old corner radius model design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,12 +70,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
         sm: 'calc(var(--radius) - 4px)',
-        xl: 'calc(var(--radius) + 4px)',
-        '2xl': 'calc(var(--radius) + 8px)',
-        '3xl': 'calc(var(--radius) + 16px)',
       },
       keyframes: {
         'accordion-down': {
