3c978f0a2c21f4e6dddce289353c94a11ba724f4
Albin Shaji <albinalby24@gmail.com>
2025-11-05 15:40:36 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'i.pravatar.cc',
         port: '',
         pathname: '/**',
@@ -38,17 +38,6 @@ const nextConfig: NextConfig = {
     ],
     unoptimized: true, // Required for data: URLs
   },
-  // Allow cross-origin requests in development from the cloud workstation
-  // See: https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins
-  ...(process.env.NODE_ENV === 'development'
-    ? {
-        experimental: {
-          allowedDevOrigins: [
-            'https://*.cluster-va5f6x3wzzh4stde63ddr3qgge.cloudworkstations.dev',
-          ],
-        },
-      }
-    : {}),
 };
 
 export default nextConfig;
